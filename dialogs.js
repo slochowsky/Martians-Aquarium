e17b507d364e3a48d57e32757ed07458faa72237
blake slochowsky <blakeslochowsky.com>
2020-02-20 14:47:49 -0600

    working events and dialog boxes

---
+++ dialogs.js
@@ -0,0 +1,64 @@
+const initializeDetailButtonEvents = () => {
+    // CAN'T TOUCH THIS - START
+    const allCloseButtons = document.querySelectorAll(".button--close")
+
+    for (const btn of allCloseButtons) {
+        btn.addEventListener(
+            "click",
+            theEvent => {
+                const dialogElement = theEvent.target.parentNode
+                dialogElement.close()
+            }
+        )
+    }
+    // CAN'T TOUCH THIS - END
+
+
+    // You will be writing code below this line
+
+    // Show Bart's details when the button is clicked
+    document.querySelector("#button--bart").addEventListener(
+        "click",
+        theClickEvent => {
+            const theDialog = document.querySelector("#details--bart")
+            theDialog.showModal()
+        }
+    )
+    document.querySelector("#button--jim").addEventListener(
+        "click",
+        theClickEvent => {
+            const theDialog = document.querySelector("#details--jim")
+            theDialog.showModal()
+        }
+    )
+    document.querySelector("#button--frank").addEventListener(
+        "click",
+        theClickEvent => {
+            const theDialog = document.querySelector("#details--frank")
+            theDialog.showModal()
+        }
+    )
+    document.querySelector("#button--ben").addEventListener(
+        "click",
+        theClickEvent => {
+            const theDialog = document.querySelector("#details--ben")
+            theDialog.showModal()
+        }
+    )
+    document.querySelector("#button--lilly").addEventListener(
+        "click",
+        theClickEvent => {
+            const theDialog = document.querySelector("#details--lilly")
+            theDialog.showModal()
+        }
+    )
+    document.querySelector("#button--susan").addEventListener(
+        "click",
+        theClickEvent => {
+            const theDialog = document.querySelector("#details--susan")
+            theDialog.showModal()
+        }
+    )
+
+}
+export default initializeDetailButtonEvents
---
+++ main.js
@@ -0,0 +1,3 @@
+import initializeDetailButtonEvents from "./dialogs.js"
+
+initializeDetailButtonEvents()
